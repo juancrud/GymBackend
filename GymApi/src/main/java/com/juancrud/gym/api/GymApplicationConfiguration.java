bbd2ed75694c85d3e34aa12353d9dc6399c3c95c
Juan Carlos Rudin <juan.rudin@drivedominion.com>
2018-08-08 12:12:28 -0600

    CORS fix.

--- GymApi/src/main/java/com/juancrud/gym/api/GymApplicationConfiguration.java
+++ GymApi/src/main/java/com/juancrud/gym/api/GymApplicationConfiguration.java
@@ -19,7 +19,7 @@ public class GymApplicationConfiguration {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
 			}
 		};
 	}
